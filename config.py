6ea3250414594866428b40e0c02c0194b2bd6c75
Phillip Cheng <phillip.cheng@usc.edu>
2015-09-19 09:54:21 -0700

    Add back secret key config to config.py

--- config.py
+++ config.py
@@ -1,16 +1,34 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+
 class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     REPORT_DATABASE = os.path.join(basedir,'..','core','powerscribe','reportdiff_ps.db')
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir,'..','core','powerscribe','users.db')
     
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') 
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 't0p s3cr3t'
+    #SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+    #    'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+
+class TestingConfig(Config):
+    TESTING = True
+    SECRET_KEY = 'secret'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+
 
 config = {
     'development': DevelopmentConfig,
-    'default': Config
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+
+    'default': DevelopmentConfig
 }
 
