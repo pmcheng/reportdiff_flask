f556d8615741f4d870aabee221257cc15f666de5
Phillip Cheng <phillip.cheng@usc.edu>
2015-10-24 06:47:47 -0700

    Clean up commented direct SQLite access code

--- app/reports/routes.py
+++ app/reports/routes.py
@@ -1,5 +1,4 @@
 import sqlite3,os
-#from flask import g, current_app
 
 from flask import render_template, flash, redirect, url_for
 from flask.ext.login import current_user
@@ -10,42 +9,17 @@ import datetime
 import diff_match_patch
 from sqlalchemy import or_, and_, func
  
-#def get_db():   
-#    rdb = getattr(g, '_database', None)
-#    if rdb is None:
-#        rdb = g._database = sqlite3.connect(current_app.config['REPORT_DATABASE'])
-#    rdb.row_factory = sqlite3.Row
-#    return rdb
-#
-#def query_db(query, args=(), one=False):
-#    cur = get_db().execute(query, args)
-#    rv = cur.fetchall()
-#    cur.close()
-#    return (rv[0] if rv else None) if one else rv    
-#
-#@reports.teardown_request
-#def close_connection(exception):
-#    rdb = getattr(g, '_database', None)
-#    if rdb is not None:
-#        rdb.close()
-
 @reports.route('/')
 def index():
     if current_user.is_authenticated():
-        #querystring="attendingID={0} or residentID={0}".format(current_user.ps_id)
-
-        #numreports=query_db("select count(*) as count from study where "+querystring,one=True)[0]
         numreports=db.session.query(func.count(Report.accession)).filter(or_(Report.attendingID==current_user.ps_id, Report.residentID==current_user.ps_id)).first()[0]
         
-        #lastreport=query_db("select max(timestamp) as lasttime from study where final is not null and ("+querystring+")",one=True)[0]
         lastreport=db.session.query(func.max(Report.timestamp)).filter(and_(Report.final!=None,or_(Report.attendingID==current_user.ps_id, Report.residentID==current_user.ps_id))).first()[0]
-        #avscore=query_db("select avg(diff_score_percent) as avscore from study where "+querystring,one=True)[0]
+
         if numreports>0: 
             avscore=db.session.query(func.avg(Report.diff_score_percent)).filter(or_(Report.attendingID==current_user.ps_id, Report.residentID==current_user.ps_id)).first()[0]
         else:
             avscore=0
-        
-        #histoquery="select case "
         
         bars=11
         categories=[]
@@ -61,16 +35,8 @@ def index():
         for i in range(bars-1):
             limit_low=i*barsize
             limit_high=limit_low+barsize
-            #histoquery+="when diff_score_percent>={0} and diff_score_percent<{1} then {2} ".format(limit_low,limit_high,i)
             categories.append("{0}-{1}%".format(limit_low,limit_high))
-        #histoquery+="when diff_score_percent>=100 then {0} ".format(bars-1)
         categories.append(">100%")
-        #histoquery+="end as bin, count(1) as c from study where {0} group by bin order by bin".format(querystring)
-        #bins=query_db(histoquery)
-        
-        #for item in bins:
-        #    if item[0] is not None:
-        #        data[item[0]]=item[1]
                 
         histogram_json={
             'chart': {'type':'column'},
@@ -88,8 +54,6 @@ def index():
     else:
         return render_template('reports/index.html')
 
-#def buildquery(userstring):
-#    return "attending like '%"+userstring+"%' or resident like '%"+userstring+"%'"
 
 @reports.route('/user/<username>/comparison')
 def comparison(username):
@@ -97,23 +61,18 @@ def comparison(username):
     if user!=current_user: 
         flash('Cannot access requested page.')
         return redirect(url_for('reports.index'))
-    #attendingquery="select attendingID, avg(diff_score_percent) from study where attendingID is not NULL group by attendingID"
-    #attendingresult=query_db(attendingquery)
+
     attendingresult=db.session.query(Report.attendingID, func.avg(Report.diff_score_percent).label('average')).filter(Report.attendingID!=None).group_by(Report.attendingID)
     attendingscores={} # (attendingID) => average diff_score_percent
     for item in attendingresult:
         attendingscores[int(item.attendingID)]=float(item.average)
     attendingsorted=sorted(attendingscores.items(),key=lambda x:x[1],reverse=True)
     
-    #studyquery="select residentID, attendingID, sum(diff_score_percent), count(*) as count from study where residentID is not NULL and attendingID is not NULL group by residentID, attendingID"
-    #studyresult=query_db(studyquery)
     studyresult=db.session.query(Report.residentID, Report.attendingID, func.sum(Report.diff_score_percent).label('sum'), func.count(Report.diff_score_percent).label('count')).filter(and_(Report.attendingID!=None,Report.residentID!=None)).group_by(Report.residentID,Report.attendingID)
     studyscores={}  # (residentID, attendingID) => total diff_score_percent
     studycounts={}  # (residentID, attendingID) => study count
     for item in studyresult:
-        #studyscores[(int(item[0]),int(item[1]))]=float(item[2])
         studyscores[(int(item.residentID),int(item.attendingID))]=float(item.sum)
-        #studycounts[(int(item[0]),int(item[1]))]=float(item[3])
         studycounts[(int(item.residentID),int(item.attendingID))]=int(item.count)
     
     for key in studyscores:
@@ -133,8 +92,6 @@ def comparison(username):
             by_attending[str(key[1])]=studyscores[key]/studycounts[key]
     
     data=sorted(residentscores.items(),key=lambda x:x[1],reverse=True)
-    
-    
     
     resident_names=[]
     for item in data:
@@ -193,9 +150,6 @@ def user(username):
         flash('Cannot access requested page.')
         return redirect(url_for('reports.index'))
         
-    #querystring="attendingID={0} or residentID={0}".format(current_user.ps_id)
-    #query="select accession, timestamp, proceduredescription, attending, resident, diff_score_percent from study where diff_score is not null and ("+querystring+") order by timestamp desc"
-    #reports=query_db(query)
     reports=Report.query.filter(Report.diff_score!=None).filter(or_(Report.attendingID==current_user.ps_id, Report.residentID==current_user.ps_id)).order_by(Report.timestamp.desc())
     return render_template('reports/user.html',user=user,reports=reports)
 
@@ -205,18 +159,15 @@ def accession(accession):
         flash('Cannot access requested page.')
         return redirect(url_for('reports.index'))
     
-    #report=query_db("select * from study where accession like '%s'" % accession, one=True)
     report=Report.query.filter(Report.accession==accession).first()
     
     if report is None:
         return redirect('/user/'+current_user.username)
 
-    #if report["prelim"] is None or report["final"] is None:
     if report.prelim is None or report.final is None:
         return redirect('/user/'+current_user.username)
        
     # Need to be an author of the report to view it
-    #if report["attendingID"]!=current_user.ps_id and report["residentID"]!=current_user.ps_id:
     if report.attendingID!=current_user.ps_id and report.residentID!=current_user.ps_id:
         return redirect('/user/'+current_user.username)
 
--- config.py
+++ config.py
@@ -4,7 +4,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    REPORT_DATABASE = os.path.join(basedir,'..','core','powerscribe','reportdiff_ps.db')
     
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir,'..','core','powerscribe','users.db')
     SQLALCHEMY_BINDS = {
