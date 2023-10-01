e394dc0241a232874651e987bdd25211d2d3d439
Phillip Cheng <phillip.cheng@usc.edu>
2023-09-30 20:42:33 -0700

    Enforce specific versions in requirements.txt due to breaking changes in dependencies.

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -5,17 +5,17 @@ except ImportError:
     from flask_wtf import Form as FlaskForm # Fallback to Flask-WTF v0.12 or older
 
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import Required
+from wtforms.validators import InputRequired
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[Required()])
-    password = PasswordField('Password', validators=[Required()])
+    username = StringField('Username', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired()])
     remember_me = BooleanField('Keep me logged in')
     submit = SubmitField('Log In')
 
 class PasswordForm(FlaskForm):
-    old_password = PasswordField('Old Password', validators=[Required()])
-    new_password1 = PasswordField('New Password', validators=[Required()])
-    new_password2 = PasswordField('Retype New Password', validators=[Required()])
+    old_password = PasswordField('Old Password', validators=[InputRequired()])
+    new_password1 = PasswordField('New Password', validators=[InputRequired()])
+    new_password2 = PasswordField('Retype New Password', validators=[InputRequired()])
     submit = SubmitField('Submit')
